e1826a4d82acde1046b0a8010e9fce291dc4476e
Rob Pearson <rob.pearson@me.com>
2018-06-11 21:31:37 +1000

    Fixed bug to match quotes with author and shuffled functions a bit.

--- source/server/QuotesController.js
+++ source/server/QuotesController.js
@@ -3,27 +3,26 @@ var router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-
-function LoadData(filename) {
-    return new Promise((res, rej) => fs.readFile(path.join(__dirname, "data",filename), "UTF-8", (err, data) => {
-        if(err){
-            rej();
-        } else {
-            res(data.split("\r\n"))
-        }
-    }));
-}
-
 function RandomQuote() {
     return LoadData("quotes.txt").then((quotes) =>
         LoadData("authors.txt").then(authors => {
+            let randomQuoteIndex = getRandomInt(quotes.length);
             return {
-                quote: quotes[getRandomInt(quotes.length)],
-                author: authors[getRandomInt(authors.length)]
+                quote: quotes[randomQuoteIndex],
+                author: authors[randomQuoteIndex]
             };
         }));
 }
 
+function LoadData(filename) {
+    return new Promise((res, rej) => fs.readFile(path.join(__dirname, "data", filename), "UTF-8", (err, data) => {
+        if (err) {
+            rej();
+        } else {
+            res(data.split(/[\r\n]+/))
+        }
+    }));
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
